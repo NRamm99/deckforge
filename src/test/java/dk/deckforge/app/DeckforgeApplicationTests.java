250fd64804e976c706660c10799f448e41baee82
Nichlas Ramm <162293775+NRamm99@users.noreply.github.com>
2026-05-05 19:02:42 +0200

    Initial commit

---
+++ src/test/java/dk/deckforge/app/DeckforgeApplicationTests.java
@@ -0,0 +1,13 @@
+package dk.deckforge.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeckforgeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
