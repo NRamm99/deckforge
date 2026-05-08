d46a70320716c084099d202b9eab284117322bfb
Nichlas Ramm <162293775+NRamm99@users.noreply.github.com>
2026-05-08 21:53:36 +0200

    add Card class

---
+++ src/main/java/dk/deckforge/app/domain/model/Card.java
@@ -0,0 +1,10 @@
+package dk.deckforge.app.domain.model;
+
+public class Card {
+    long id;
+    String name;
+    String description;
+
+    // undersøg hvilke att kort skal have. Jeg tænker ikke vi benytter os af api eller scraping
+    // så vi skal selv forholde os til hvad et kort skal indeholde
+}
