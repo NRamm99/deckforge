594d3e6c14f9ce7c983be04f6661acec40e82711
NSJessen <niklas.spanggaard.jessen@gmail.com>
2026-05-11 18:23:02 +0200

    add card class and enums

---
+++ src/main/java/dk/deckforge/app/domain/model/CardColor.java
@@ -0,0 +1,9 @@
+package dk.deckforge.app.domain.model;
+
+public enum CardColor {
+    WHITE,
+    BLACK,
+    BLUE,
+    GREEN,
+    RED
+}
---
+++ src/main/java/dk/deckforge/app/domain/model/CardType.java
@@ -0,0 +1,11 @@
+package dk.deckforge.app.domain.model;
+
+public enum CardType {
+    CREATURE,
+    LAND,
+    INSTANT,
+    SORCERY,
+    ARTIFACT,
+    ENCHANTMENT,
+    PLANESWALKER
+}
