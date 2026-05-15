f41aca56779d94277042e3a2c9c085f413bed1e4
Nichlas Ramm <162293775+NRamm99@users.noreply.github.com>
2026-05-15 19:20:51 +0200

    Create data.sql

---
+++ src/main/resources/data.sql
@@ -0,0 +1,48 @@
+INSERT INTO card (id, name, description, card_set, rarity, card_type, color, image_url, mana) VALUES
+(1, 'Agent Bishop, Man in Black', 'Test', 'mega sejt', 'RARE', 'LAND', 'WHITE', 'https://gatherer-static.wizards.com/Cards/medium/B28670B74E9139BB198AC5A0CC647F5EE54CB4C4D4805BCAC78AE4AF8D1BD301.webp', '2G'),
+(2, 'Agent of the Iron Throne', 'Commander creatures you own have "Whenever an artifact or creature you control is put into a graveyard from the battlefield, each opponent loses 1 life."
+
+"You give your clients what they want at a fair price. What happens after that is none of your concern."', 'Set CLB - Commander Legends: Battle for Baldur''s Gate', 'UNCOMMON', 'ENCHANTMENT', 'BLACK', 'https://gatherer-static.wizards.com/Cards/medium/F93098277C4337BAF2E2431FBD81DE492AFF04C2ABA794DCE2D216A281D50F0B.webp', '2D'),
+(3, 'Agent of Treachery', 'When this creature enters, gain control of target permanent.
+At the beginning of your end step, if you control three or more permanents you don''t own, draw three cards.', 'Set TLE - Magic: The Gathering | Avatar: The Last Airbender Extra', 'MYTHIC_RARE', 'CREATURE', 'BLUE', 'https://gatherer-static.wizards.com/Cards/medium/4D5107A47249ED1E37D87338DABE7589C68EA21AA69704FF8905278F65EA583D.webp', '5W'),
+(4, 'Staff of the Storyteller', 'When this artifact enters, create a 1/1 white Spirit creature token with flying.
+Whenever you create one or more creature tokens, put a story counter on this artifact.', 'ONC - Phyrexia: All Will Be One Commander', 'RARE', 'ARTIFACT', 'BLACK', 'https://gatherer-static.wizards.com/Cards/medium/9F59F06B3C4FA5EEF882B0680ACE748BFE04BD181B5656466B928373957FB15B.webp', '1S'),
+(5, 'Kellan, Planar Trailblazer', '1
+: If Kellan is a Scout, it becomes a Human Faerie Detective and gains "Whenever Kellan deals combat damage to a player, exile the top card of your library. You may play that card this turn."
+2
+: If Kellan is a Detective, it becomes a 3/2 Human Faerie Rogue and gains double strike.', 'FDN - Magic: The Gathering Foundations', 'RARE', 'CREATURE', 'RED', 'https://gatherer-static.wizards.com/Cards/medium/4F11289348F328B71F222D7607BD4C739A8288FE7BEDD8C7E5ECD65D1AD9F0F6.webp', '2F'),
+(6, 'Quintorius, History Chaser', 'Whenever one or more cards leave your graveyard, create a 3/2 red and white Spirit creature token.
++1: You may discard a card. If you do, draw two cards, then mill a card.
+?4: Spirits you control gain double strike and vigilance until end of turn.
+Quintorius, History Chaser can be your commander.', 'Set SOC - Secrets of Strixhaven Commander', 'MYTHIC_RARE', 'PLANESWALKER', 'RED', 'https://gatherer-static.wizards.com/Cards/medium/B241165134AF4B3D1420CA61EE8DC19D6291029D65B84C881C30CE1DF27DB1C4.webp', '2FS'),
+(7, 'Excava, the Risen Past', 'Flying, haste
+Whenever Excava attacks, return up to one target artifact, creature, or non-Aura enchantment card with mana value 3 or less from your graveyard to the battlefield with a finality counter on it. It''s a 1/1 Spirit creature with flying in addition to its other types. (If a creature with a finality counter on it would die, exile it instead.)', 'SOC - Secrets of Strixhaven Commander', NULL, 'CREATURE', 'RED', 'https://gatherer-static.wizards.com/Cards/medium/55A0EFDD71FBC98B2967A29C06BC226FBD460D1B04BCFE5B9EFF505BF64BD2A7.webp', '2FS'),
+(8, 'Killian, Decisive Mentor', 'Whenever an enchantment you control enters, tap up to one target creature and goad it. (Until your next turn, that creature attacks each combat if able and attacks a player other than you if able.)
+Whenever one or more creatures that are enchanted by an Aura you control attack, draw a card.', 'SOC - Secrets of Strixhaven Commander', NULL, 'CREATURE', 'WHITE', 'https://gatherer-static.wizards.com/Cards/medium/5E0F0A6D9F7A178EBAE05F03DC2E13E46F73ED5BFFEC9A0169AA9D6318B734EF.webp', '1SD'),
+(9, 'Scriv, the Obligator', 'Flying, deathtouch
+Whenever Scriv enters or attacks, create a white Aura enchantment token named Contract attached to target creature an opponent controls. The token has enchant creature and "Whenever enchanted creature attacks, it gets +2/+0 until end of turn if it''s attacking one of your opponents. Otherwise, its controller loses 2 life."', 'SOC - Secrets of Strixhaven Commander', 'MYTHIC_RARE', 'CREATURE', 'WHITE', 'https://gatherer-static.wizards.com/Cards/medium/1933DC0BB25E70E43BEF9A224C5ADC773B44526F4765EC0BF6E8E2A391E276B5.webp', '2SD'),
+(10, 'Nexus Mentality', 'Choose one. If you control a commander as you cast this spell, you may choose both instead.
+* Move all counters from target nonland permanent you control onto another target nonland permanent you control.
+* Remove all counters from target nonland permanent you control. Draw a card for each counter removed this way.', 'SOC - Secrets of Strixhaven Commander', 'RARE', 'INSTANT', 'BLUE', 'https://gatherer-static.wizards.com/Cards/medium/92002FD7D7D32E0CBA61F3B76BB380D933DA8F856CACBC6FFC09CA78ED3B4C42.webp', '3W'),
+(11, 'Lattice Library', 'This enchantment enters with X study counters on it.
+When this enchantment enters and whenever you cast your first spell with 
+X
+ in its mana cost each turn, create a 0/0 green and blue Fractal creature token. Put a number of +1/+1 counters on it equal to the number of study counters on this enchantment.', 'SOC - Secrets of Strixhaven Commander', 'RARE', 'ENCHANTMENT', 'GREEN', 'https://gatherer-static.wizards.com/Cards/medium/0F7EFDFFCC9309F61B7A605B7A72CAF65EA9407E53AD32513716B14D60D6A8CB.webp', 'xN'),
+(12, 'Eccentric Pestfinder', 'Trample
+At the beginning of each end step, if you gained life this turn, this creature becomes prepared. (While it''s prepared, you may cast a copy of its spell. Doing so unprepares it.)
+//Prep//
+Turn Stones', 'SOC - Secrets of Strixhaven Commander', 'RARE', 'CREATURE', 'GREEN', 'https://gatherer-static.wizards.com/Cards/medium/FCA7750AA701A765D7B0F221BF03E95326826A3AD097160DAC0A38540709025B.webp', '2ND'),
+(13, 'Vanguard of the Restless', 'Flying
+Spirits you control get +1/+1 for each time you''ve cast your commander from the command zone this game.
+Whenever a Spirit you control enters, you may pay 
+2
+. If you do, return this card from your graveyard to the battlefield.', 'SOC - Secrets of Strixhaven Commander', 'RARE', 'CREATURE', 'BLACK', 'https://gatherer-static.wizards.com/Cards/medium/03C7D3628C4BCE4E59E490800E36E68A0DA28E3B679A3CAFE14F71D274B71EA1.webp', '2S')
+ON DUPLICATE KEY UPDATE
+    name = VALUES(name),
+    description = VALUES(description),
+    card_set = VALUES(card_set),
+    rarity = VALUES(rarity),
+    card_type = VALUES(card_type),
+    color = VALUES(color),
+    image_url = VALUES(image_url),
+    mana = VALUES(mana);
